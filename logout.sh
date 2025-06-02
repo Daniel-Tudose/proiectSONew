4dd23f70bda76357ce1ad8d022025d44956d7413
Alex4805 <alextud22@gmail.com>
2025-06-02 18:47:45 +0300

    Update logout.sh

--- logout.sh
+++ logout.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+fisier_temporar="logged_users.tmp"
+
+# Verifică dacă fișierul de sesiune există
+if [ ! -f "$fisier_temporar" ]; then
+    echo "Nu exista utilizatori autentificati."
+    exit 1
+fi
+
+echo "Introdu numele de utilizator pentru logout:"
+read USERNAME
+
+
+if grep -q "^$username$" "$fisier_temporar"; then
+    sed -i "/^$username$/d" "$fisier_temporar"
+    echo "Utilizatorul '$username' a fost deconectat cu succes."
+else
+    echo "Utilizatorul '$username' nu este autentificat."
+fi
