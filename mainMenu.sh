44f8fe04558ddf5d2fddff727a3c1bce4739d66f
Alex4805 <alextud22@gmail.com>
2025-06-02 18:55:19 +0300

    Update mainMenu.sh

--- mainMenu.sh
+++ mainMenu.sh
@@ -27,8 +27,7 @@ do
   elif [ "$optiune" = "4" ]; then
     echo "Los siento. No hablas espanol, pero aqui estan los usuarios en linea: ${loggedInUsers[@]}"
   elif [ "$optiune" = "5" ]; then
-    # echo "Logging out ======== UNDER CONSTRUCTION!"
-    mapfile -t logged_in_users < logged_users.tmp
+     echo "Logging out ======== UNDER CONSTRUCTION!"
   elif [ "$optiune" = "Q" ] || [ "$optiune" = "q" ]; then
     echo "La revedere!"
     exit 0
