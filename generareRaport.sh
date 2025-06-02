158248eb9b655d996191953444fc85579c503815
Alex4805 <alextud22@gmail.com>
2025-06-02 18:13:40 +0300

    Raport.sh

--- generareRaport.sh
+++ generareRaport.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+username="$1"
+user_home="home/$username"
+raport_fisier="$user_home/raport.txt"
+
+if [ ! -d "$user_home" ]; then 
+  echo "Utilizatorul nu are directorul home."
+  exit 1
+fi
+
+(
+  echo "Raport pentru utilizatorul: $username" > "$raport_fisier"
+  echo "Generat la: $(date)" >> "$raport_fisier"
+  echo "Dimensiunea totala pe disc: $SIZE" >> "$raport_fisier"
+  echo "------------------------------------" >> "$raport_fisier"
+  echo "Raport generat cu succes." >> "$raport_fisier"
+  
+) &
+
+echo "Raportul este generat pentru utilizatorul $username."
