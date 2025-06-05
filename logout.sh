386d4d030b50635a01c7e386c346b0c440c56cd2
Tecshor <teo.alex10@gmail.com>
2025-06-05 15:03:24 +0300

    Update logout.sh

--- logout.sh
+++ logout.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# Verificăm dacă există utilizatori autentificați
 if [ ${#loggedInUsers[@]} -eq 0 ]; then
   echo "Nu există utilizatori autentificați în acest moment."
   exit 1
 fi
 
-# Preluăm ultimul utilizator autentificat
 username="${loggedInUsers[-1]}"
 
-# Creăm un array nou fără acest utilizator
 nouaLista=()
 for user in "${loggedInUsers[@]}"; do
   if [ "$user" != "$username" ]; then
@@ -17,7 +14,6 @@ for user in "${loggedInUsers[@]}"; do
   fi
 done
 
-# Actualizăm array-ul
 loggedInUsers=("${nouaLista[@]}")
 
 echo "Utilizatorul '$username' a fost delogat cu succes."
